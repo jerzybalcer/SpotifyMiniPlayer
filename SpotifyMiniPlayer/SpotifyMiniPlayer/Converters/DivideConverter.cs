4c9b68436e98d3f621e8c0aad6324de4271aa196
Jerzy <jurekbalcer@gmail.com>
2022-03-29 11:56:07 +0200

    styling buttons

---
+++ SpotifyMiniPlayer/SpotifyMiniPlayer/Converters/DivideConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SpotifyMiniPlayer.Converters
+{
+    public class DivideConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double numVal = (double)value;
+            return numVal/2;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
